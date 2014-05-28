335bd539c8dcf6ed1a98360a85a8e3acd6123760
Pedroguarana <pedro.guarana@hebara.com.br>
2014-05-28 15:40:54 -0300

    Novo Projeto

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet.People' table. You can move, or remove it, as needed.
+            this.peopleTableAdapter.Fill(this.database1DataSet.People);
+
+        }
+
+        private void peopleBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peopleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.database1DataSet);
+
+        }
+
+        private void telefoneTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clientLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clientCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
